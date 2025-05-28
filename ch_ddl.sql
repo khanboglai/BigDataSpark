b2b17049414a7812614ad9374ec33a3fc8f1520b
Aleksey <boglaev.ale@gmail.com>
2025-05-29 01:28:11 +0300

    Добавил отчеты, исправил ошибки

--- ch_ddl.sql
+++ ch_ddl.sql
@@ -20,7 +20,7 @@ CREATE TABLE report_customer
 ENGINE = MergeTree
 ORDER BY tuple();
 
-
+-- отчет по продажам по времени
 CREATE TABLE report_time
 (
 --     id UInt32,
@@ -28,4 +28,28 @@ CREATE TABLE report_time
     report_type String
 )
 ENGINE = MergeTree
+ORDER BY tuple();
+
+
+-- отчет по магазинам
+CREATE TABLE store_report (
+    report_type String,
+    report_data String
+) ENGINE = MergeTree()
+ORDER BY tuple();
+
+
+-- отчет по поставщикам
+CREATE TABLE report_supplier (
+    report_type String,
+    report_data String
+) ENGINE = MergeTree()
+ORDER BY tuple();
+
+
+-- отчет по качеству продукции
+CREATE TABLE report_quality (
+    report_type String,
+    report_data String
+) ENGINE = MergeTree()
 ORDER BY tuple();
